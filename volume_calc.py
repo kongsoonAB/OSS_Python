36fff14bd3d456da13d0a00f4ae597b7e005fe0e
kongsoonAB <76522787+kongsoonAB@users.noreply.github.com>
2023-03-16 23:03:19 +0900

    Update volume_calc.py
    오타 수정

--- volume_calc.py
+++ volume_calc.py
@@ -2,7 +2,7 @@ length = float(input("가로: "))
 width = float(input("세로: "))
 height = float(input("높이: "))
 
-volume = length * width * heigth
+volume = length * width * height
 
 
 print("박스의 부피는", volume, "입니다.")
